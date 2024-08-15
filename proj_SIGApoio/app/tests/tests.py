11491a85287528189bbed16b1141ed999d7011be
Anderson Azevedo <andersonsilva14.2017@gmail.com>
2024-08-15 16:15:57 -0300

    início dos testes

--- proj_SIGApoio/app/tests/tests.py
+++ proj_SIGApoio/app/tests/tests.py
@@ -1,1 +1,12 @@
-# Create your tests here.
+from django.test import TestCase
+
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from json import dumps
+
+
+class TestFront(TestCase):
+    def setUp(self):
+        self.client = APIClient()
