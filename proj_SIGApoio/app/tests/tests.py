eb2a9ed07277d2f4668567360800c7991b8005e4
Rafael Garcia <garciarafael.2298@gmail.com>
2024-08-15 16:52:20 -0300

    up tests #115

--- proj_SIGApoio/app/tests/tests.py
+++ proj_SIGApoio/app/tests/tests.py
@@ -10,3 +10,33 @@ from json import dumps
 class TestFront(TestCase):
     def setUp(self):
         self.client = APIClient()
+    
+    def test_cadastro_reserva_recurso_get(self):
+        res = self.client.get(reverse('reserva_recurso'))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+    
+    def test_cadastro_reserva_recurso_post(self):
+        res = self.client.post(reverse('reserva_recurso'), 
+            data={
+                'descricao':'SEMANA DA INFORMÁTICA',
+                'horarios': 'M1',
+                'dias':'1',
+                'qtd_pessoas':10,
+                'bloco':'B',
+                'local': 'B1',
+                'matSolicitante':'202401'
+            }
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+    
+    def test_listar_emprestimos_get(self):
+        res = self.client.get(reverse('listar_emprestimos'))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+    
+    def test_listar_local_get(self):
+        res = self.client.get(reverse('listar_local'))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        
+    def test_listar_recursos_get(self):
+        res = self.client.get(reverse('listar-recurso'))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
