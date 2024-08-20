a9f4433dbd170f1b017664072c7b2d9824bece64
Anderson Azevedo <andersonsilva14.2017@gmail.com>
2024-08-20 18:50:54 -0300

    up

--- proj_SIGApoio/core/settings.py
+++ proj_SIGApoio/core/settings.py
@@ -141,11 +141,14 @@ USE_TZ = True
 # ]
 # '''
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    BASE_DIR / "app/static",  # Ou o caminho para a pasta onde seus arquivos estáticos estão localizados
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "app/static",  # Ou o caminho para a pasta onde seus arquivos estáticos estão localizados
+# ]
+STATIC_URL = '/sigapoio-files/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'sigapoio/run/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
